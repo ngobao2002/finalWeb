221a6815e8d5b9f7afeec0767227ae1b7a696e77
ngobao2002 <bao.ngo200202@vnuk.edu.vn>
2022-06-19 23:19:34 +0700

    met qua

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /**First slider */
-$('.slider-one').slick();
+$('.slider-one')
+.not(".slick-intialized")
+.slick({
+    autoplay: true,
+    autoplaySpeed:3000,
+    dots: true,
+    prevArrow:".site-slider .slider-btn .prev",
+    nextArrow:".site-slider .slider-btn .next",
+});
