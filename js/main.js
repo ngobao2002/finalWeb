61bf5f961b924095b973b890af0bb9c7cf1e4b99
ppn243 <nguyenwin2432002@gmail.com>
2022-06-24 20:17:15 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -8,3 +8,30 @@ $('.slider-one')
     prevArrow:".site-slider .slider-btn .prev",
     nextArrow:".site-slider .slider-btn .next",
 });
+
+var swiper = new Swiper(".games", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
