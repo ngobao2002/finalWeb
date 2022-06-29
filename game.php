326c8d43ec4a8fd43e20d2525c4cfd9679c33d03
truong <truong.nguyen200207@vnuk.edu.vn>
2022-06-29 21:28:54 +0700

    continue

--- game.php
+++ game.php
@@ -1,97 +1,186 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <link href="css/game.css" rel="stylesheet" type="text/css"/>
-    <script src="./js/main.js"></script>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
-  </head>
-  <body class="bg-dark">
-    <h1 class="mx-auto pt-3" style="width: 300px;text-align: center; color: azure; padding-bottom: 25px;">God of War</h1>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-9" class="float-start">
-          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true" class="float-start">
-            <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="50000">
-                <video width="100%" height="auto" autoplay controls muted>
-                  <source src="img/game-video.mp4" type="video/mp4">
-                </video>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <link href="./css/game.css" rel="stylesheet" type="text/css"/>
+  <script src="./js/main.js"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  
+</head>
+<body class="bg-primary-color">
+  <h1 class="mx-auto p-3" style="width: 300px;text-align: center; color: azure; padding-bottom: 25px; size: 200px;">God of War</h1>
+  <div class="container">
+    <div class="row ">
+      <div class="col-md-9 pb-2 " class="float-start">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true" class="float-start">
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="">
+              <img src="./img/gow1.jpg" class="d-block w-100" alt="...">
+            </div>
+              <div class="carousel-item">
+                <img src="./img/gow2.jpg" class="d-block w-100" alt="...">
               </div>
-                <div class="carousel-item">
-                  <img src="./img/game.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="./img/game2.jpg" class="d-block w-100" alt="...">
-                </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-      </div>
-      <div class="text font-roboto"> 
-        <h5> Enter the Norse realm</h5>
-        <p>His vengeance against the Gods of Olympus years behind him, Kratos now lives as a man in the realm of Norse Gods and monsters. It is in this harsh, unforgiving world that he must fight to survive… and teach his son to do the same.</p>
-        <h5> Grasp a second chance</h5>
-        <p>Kratos is a father again. As mentor and protector to Atreus, a son determined to earn his respect, he is forced to deal with and control the rage that has long defined him while out in a very dangerous world with his son.</p>
-        <h5></h5>
-      </div>
-      <ul class="nav nav-tabs">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
+              <div class="carousel-item">
+                <img src="./img/gow3.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="./img/gow4.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="./img/gow5.jpg" class="d-block w-100" alt="...">
+              </div>
+              
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    
+    <div class="text font-roboto"> 
+      <h5>Enter the Norse realm</h5>
+      <p>His vengeance against the Gods of Olympus years behind him, Kratos now lives as a man in the realm of Norse Gods and monsters. It is in this harsh, unforgiving world that he must fight to survive… and teach his son to do the same.</p>
+      <h5> Grasp a second chance</h5>
+      <p>Kratos is a father again. As mentor and protector to Atreus, a son determined to earn his respect, he is forced to deal with and control the rage that has long defined him while out in a very dangerous world with his son.</p>
+    </div>
+    <div id="more-text" class="text font-roboto text-hide hidden-md">
+      <h5>Journey to a dark, elemental world of fearsome creatures</h5>
+      <p>From the marble and columns of ornate Olympus to the gritty forests, mountains and caves of pre-Viking Norse lore, this is a distinctly new realm with its own pantheon of creatures, monsters and gods.</p>
+      <h5>Engage in visceral, physical combat</h5>
+      <p>With an over the shoulder camera that brings the player closer to the action than ever before, fights in God of War™ mirror the pantheon of Norse creatures Kratos will face: grand, gritty and grueling. A new main weapon and new abilities retain the defining spirit of the God of War series while presenting a vision of conflict that forges new ground in the genre.</p>
+    </div>
+    <button id="show-text" type="button" class="btn btn-primary mb-2" >Show/Hide</button>
 
-          <div class="col-md-3">
-            <img id="main-img" src="./img/game1.jpg" width="100%" >
-            <div id="price">
-              <span >$39.99</span>
+    <h1 style="color:white ;width: 300px;padding: 25px; size: 200px;">Specifications</h1>
+
+    <ul class="nav nav-tabs">
+      <li class="nav-item" role="Windows">
+      <button class="nav-link active" id="windows-tabs" data-bs-toggle="tab" data-bs-target="#windows" type="button" role="tab" aria-controls="windows" aria-selected="true">Windows</button>
+      </li>
+      <li class="nav-item" role="masOS">
+        <button class="nav-link" id="masOS-tab" data-bs-toggle="tab" data-bs-target="#masOS" type="button" role="tab" aria-controls="masOS" aria-selected="false">MasOS</button>
+      </li>
+    </ul>
+    <div class="" style="background-color: #1B1B1B;">
+      <div class="tab-content">
+        <div class="tab-pane fade show active" id="windows" role="tabpanel" aria-labelledby="home-tab">
+          <div class="container">
+            <div class="row" style="color:azure">
+              <div class="col p-3">
+                <h5 class="pb-3 text-center">Minimum</h5>
+                <div class="pb-2">
+                  <h5>OS</h5>
+                  <p>Windows 10 64-bit</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Processcer</h5>
+                  <p>Intel i5-2500k (4 core 3.3 GHz) or AMD Ryzen 3 1200 (4 core 3.1 GHz)</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Memory</h5>
+                  <p>8 GB DDR</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Storage</h5>
+                  <p>70 GB HDD (SSD Recommended)</p>
+                </div>   
+                <div class="pb-2">
+                  <h5>Direct X</h5>
+                  <p>DirectX 11 (feature level 11_1)</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Graphics</h5>
+                  <p>NVIDIA GTX 960 (4 GB) or AMD R9 290X (4 GB)</p>
+                </div>             
+              </div>
+              <div class="col p-3">
+                <h5 class="pb-3 text-center">Recommended</h5>
+                <div class="pb-2">
+                  <h5>OS</h5>
+                  <p>Windows 10 64-bit</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Processor</h5>
+                  <p>Intel i5-6600k (4 core 3.5 GHz) or AMD Ryzen 5 2400 G (4 core 3.6 GHz)</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Memory</h5>
+                  <p>8 GB DDR</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Storage</h5>
+                  <p>70 GB SSD</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Direct X</h5>
+                  <p>DirectX 11 (feature level 11_1)</p>
+                </div>
+                <div class="pb-2">
+                  <h5>Graphics</h5>
+                  <p>NVIDIA GTX 1060 (6 GB) or AMD RX 570 (4 GB)</p>
+                </div>
+              </div>
+              <h6>Languages Supported</h6>
+              <p><span>Audio: </span>English, French, German, Italian, Japanese, Polish, Portuguese, Portuguese - Brazil, Russian, Spanish - Spain, Spanish - Latin America</p>
+              <p><span>Text: </span>English, Dutch, French, German, Italian, Japanese, Korean, Polish, Portuguese, Portuguese - Brazil, Russian, Spanish - Spain, Spanish - Latin America, Turkish, Chinese - Traditional, Hungarian, Czech, Greek</p>
             </div>
-            <a class="btn btn-primary " href="#" role="button"><strong>BUY NOW</strong></a>
-            <button type="button" class="btn btn-outline-secondary btn-block btn-md"><strong>ADD TO</strong></button>
-            
-            <!--Flex Bootstrap 5-->
-            <div class="d-flex flex-column bd-highlight md-3 font-roboto">
-              <div class="border-bottom border-secondary d-flex justify-content-between">
-                <h5 class="text-start">Developer</h5> 
-                <h5 class="text-end">Santa Monica Studio</h5> 
-              </div>
-              <div class="border-bottom border-secondary d-flex justify-content-between">
-                <h5 class="text-start">Publisher</h5> 
-                <h5 class="text-end">PlayStation PC LLC</h5> 
-              </div>
-              <div class="border-bottom border-secondary d-flex justify-content-between">
-                <h5 class="text-start">Release Date</h5> 
-                <h5 class="text-end">01/14/22</h5> 
-              </div>
-              <div class="border-bottom border-secondary d-flex justify-content-between">
-                <h5 class="text-start">Platform</h5> 
-                <h5 class="text-end">Windows</h5> 
-              </div>
-            </div>
-          </div>
           </div>
         </div>
       </div>
+      <div class="tab-pane fade" id="masOS" role="tabpanel" aria-labelledby="masOS-tab" style="color:azure">
+        <h5 class="text-center p-3">None</h5>
+      </div>
     </div>
-  </body>
+  </div>
+  
+
+        <div class="col-md-3 ">
+          <img id="main-img" src="./img/game1.jpg" width="100%" >
+          <div id="price">
+            <span >$39.99</span>
+          </div>
+          <a class="btn btn-primary " href="#" role="button"><strong>BUY NOW</strong></a>
+          <button type="button" class="btn btn-outline-secondary btn-block btn-md"><strong>ADD TO CART</strong></button>
+          
+          <!--Flex Bootstrap 5-->
+          <div class="d-flex flex-column bd-highlight md-3 font-roboto">
+            <div class="border-bottom border-secondary d-flex justify-content-between">
+              <h5 class="text-start">Developer</h5> 
+              <h5 class="text-end">Santa Monica Studio</h5> 
+            </div>
+            <div class="border-bottom border-secondary d-flex justify-content-between">
+              <h5 class="text-start">Publisher</h5> 
+              <h5 class="text-end">PlayStation PC LLC</h5> 
+            </div>
+            <div class="border-bottom border-secondary d-flex justify-content-between">
+              <h5 class="text-start">Release Date</h5> 
+              <h5 class="text-end">01/14/22</h5> 
+            </div>
+            <div class="border-bottom border-secondary d-flex justify-content-between">
+              <h5 class="text-start">Platform</h5> 
+              <h5 class="text-end">Windows</h5> 
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</body>
+<script>
+  $(document).ready(function(){
+    $("#show-text").click(function(){
+      $("#more-text").toggle(5000);
+    });
+  });
+</script>
 </html>
